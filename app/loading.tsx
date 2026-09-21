29dea7d38e4b90bf6e868581db8bc004e9272815
Pau Bassols Ros <paubassols@MacBook-Air-de-Pau-2.local>
2026-09-21 18:08:25 +0200

    Actualizada pantalla de carga con botella y modal PWA idéntico a la guía

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+'use client';
 export default function Loading() {
   return (
     <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-[#f4f5f8]">
